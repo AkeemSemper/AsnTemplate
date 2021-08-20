7c48fe957beef29cf34cd413337af57621192f26
AkeemSemper <55889346+AkeemSemper@users.noreply.github.com>
2021-08-20 09:20:54 -0600

    second test added

--- test_harness.py
+++ test_harness.py
@@ -1,10 +1,13 @@
 import thinkplot
 import thinkstats2
-import sys
-sys.path.append(".")
+
 import import_ipynb
 import solution_file
 
 def test_sampleFunction():
     correctAnswer = 7
-    assert correctAnswer == sampleFunction() 
+    assert correctAnswer == solution_file.sampleFunction() 
+
+def test_secondFunction():
+    incorrectAnswer = 9
+    assert incorrectAnswer == solution_file.secondFunction()
